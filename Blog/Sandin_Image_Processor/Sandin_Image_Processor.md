9370725f22925acec7247cad0e10412ab61dc12d
Juan Eduardo <juanedflores@gmail.com>
2022-08-01 20:27:51 -0500

    updated entry post script

---
+++ Blog/Sandin_Image_Processor/Sandin_Image_Processor.md
@@ -0,0 +1,6 @@
+---
+title: Sandin Image Processor
+date_created: 2022-08-01
+---
+
+hello?
--- Blog/newBlogEntry.py
+++ Blog/newBlogEntry.py
@@ -17,7 +17,9 @@ from pathlib import Path
 
 # get todays date
 today = date.today()
+formatted_date = today.strftime("%B %d, %Y")
 last_modified = str(today)
+last_modified_formatted = str(formatted_date)
 
 # init values
 blogEntryTitle = ""
@@ -29,7 +31,7 @@ added_new_tag = False
 
 
 def main():
-    global last_modified, blogEntryTitle, blogEntryDescription, blogEntryTags, tags, inputTitle, added_new_tag
+    global last_modified, blogEntryTitle, blogEntryDescription, blogEntryTags, tags, inputTitle, added_new_tag, last_modified_formatted
 
     # If an argument was passed, we are updating, else we are creating
     if (len(sys.argv) > 1):
@@ -39,18 +41,25 @@ def main():
         sys.exit(0)
 
     else:
+        # gets the title
         blogEntryTitle = input("Enter new blog entry title: ")
+        # gets the blog entry description
         blogEntryDescription = input(
             "Enter a short description for {}: ".format(blogEntryTitle))
+        # gets the tags
         blogEntryTags = input("Enter any tags followed by comma: ")
+        # make an array of tags
         tags = blogEntryTags.split(",")
+        # create the necessary directories
         os.mkdir(blogEntryTitle.replace(" ", "_"))
         os.mkdir(blogEntryTitle.replace(" ", "_") + "/cover")
+        # create the new markdown file
         f = open("{}/{}.md".format(blogEntryTitle.replace(" ", "_"),
                                    blogEntryTitle.replace(" ", "_")), "w+")
+        # write the header
         f.write(
-            "---\ntitle: {}\ndate_created: {}\n---".format(blogEntryTitle, last_modified))
-
+            "---\ntitle: {}\ndate_created: {}\n---".format(blogEntryTitle, last_modified_formatted))
+        # 
         addEntrytoHTML()
 
         if (added_new_tag):
@@ -85,7 +94,6 @@ def main():
 
 def updateEntry():
     global blogEntryTitle, blogEntryDescription, blogEntryTags, last_modified, tags, today
-
     htmlFile = open("../blog.html", "r").read()
     entries_raw = re.findall(
         '<!-- BLOG ENTRIES -->(.*?)<!-- BLOG ENTRIES END -->', htmlFile, re.DOTALL)
@@ -160,42 +168,58 @@ def updateEntry():
 
 
 def addEntrytoHTML():
-    # global blogEntryTitle, blogEntryDescription, blogEntryTags, last_modified, tags
+    global blogEntryTitle, blogEntryDescription, blogEntryTags, last_modified, tags
 
+    # copy the blog.html file
+    os.system("cp ../blog.html ../blog_copy.html")
+    # reads the blog.html file
     htmlFile = open("../blog.html", "r").read()
+    # finds the blog entries section
     entries_raw = re.findall(
         '<!-- BLOG ENTRIES -->(.*?)<!-- BLOG ENTRIES END -->', htmlFile, re.DOTALL)
+    # splits the blog entries section into lines
     lineiterator = iter(entries_raw[0].splitlines())
+    # instantiates an array that holds each entry
     entries = []
-
+    # instantiates a string that holds the new entry
     template = "\n\n<!-- {} -->\n".format(blogEntryTitle)
+    # cleans any possible unwanted text in tags
     tagfilter = ""
     for tag in tags:
+        # remove any leading and trailing whitespace characters
         tag = tag.strip()
+        # replace spaces with underscores
         tag = tag.replace(" ", "_")
+        # prepend the tag with text to follow convention
         tagfilter += "tag-" + tag + " "
 
     print("adding tags: " + tagfilter)
-    template += "<li class='{}'>".format(tagfilter)
+
+    # create the html block
+    template += "<li class='{}' style='padding-top: 20px'>".format(tagfilter)
+    template += "<div class='entry'>"
+    template += "<div class='uk-card uk-card-default uk-grid-collapse uk-transition-toggle uk-child-width-1-2@s uk-margin' "
+    template += "onclick=" + "\"location.href='./Blog/{}/{}.html'\"".format(
+        blogEntryTitle.replace(" ", "_"), blogEntryTitle.replace(" ", "_"))
+    template += "style='border: 1px solid #000000; border-radius: 3px' uk-grid>"
+    template += "<div class='uk-flex-last@s uk-card-media-right uk-transition-opaque uk-transition-scale-up uk-cover-container'>"
+    template += "<img class='uk-cover' style='width: 100%; height: 100%; object-fit: cover' "
+    template += "src='./Blog/{}/cover/cover.png' ".format(
+        blogEntryTitle.replace(" ", "_"), blogEntryTitle.replace(" ", "_"))
+    template += "alt='' />"
+    template += "<canvas width='600' height='400'></canvas></div>"
+    template += "<div><div class='uk-card-body'>"
+    template += "<h3 class='uk-card-title'>{}</h3>".format(blogEntryTitle)
+    template += "<span>last modified: {}</span>".format(last_modified)
+    template += "<p style='color: black'>{}</p>".format(blogEntryDescription)
+    # add tags
+    template = addTags(template)
+    template += "<br /><br />"
     template += "<a href='https://juanedflores.com/Blog/{}/{}.html#disqus_thread'>Link</a>".format(
         blogEntryTitle.replace(" ", "_"), blogEntryTitle.replace(" ", "_"))
-    template += "<a href='./Blog/{}/{}.html'>\n".format(
-        blogEntryTitle.replace(" ", "_"), blogEntryTitle.replace(" ", "_"))
-    template += "<div class='uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin' uk-grid>"
-    template += "<div class='uk-background-cover' uk-img> <div class='uk-card-body'> <h3 class='uk-card-title'> {} <span class='uk-text-warning'>(WIP)</span> </h3> <span>last modified: {} </span> <p> {} </p>".format(
-        blogEntryTitle, last_modified, blogEntryDescription)
+    template += "</div></div></div></div></li>\n\n"
 
-    #  add tags
-    template = addTags(template)
-
-    template += "</div></div>"
-    template += "<div class='uk-card-media-left uk-cover-container'>"
-    template += "<img src='./Blog/{}/cover/cover.png' alt='' uk-cover />".format(
-        blogEntryTitle.replace(" ", "_"))
-    template += "<canvas width='600' height='400'></canvas>"
-    template += "</div>"
-    template += "</div> </a></li>\n\n"
-
+    # adds this new html block to the end of the blog entries section
     entries.append(template)
 
     cur_entry = ""
@@ -217,13 +241,13 @@ def addEntrytoHTML():
 
     sep = '<!-- BLOG ENTRIES -->'
     stripped = htmlFile.split(sep, 1)[
-        0] + sep + "<ul class='js-filter uk-list uk-width-2-3@s uk-align-center'>"
+        0] + sep + "<ul class='js-filter uk-list uk-width-2-3@l uk-align-center'>"
 
     for entry in entries:
         stripped += entry + "\n"
 
     stripped += "<!-- BLOG ENTRIES END -->"
-    stripped += "</ul></div></div><script id='dsq-count-scr' src='//juanedflores-blog.disqus.com/count.js' async></script></body></html>"
+    stripped += "</ul><script id='dsq-count-scr' src='//juanedflores-blog.disqus.com/count.js' async></script></div></div></body></html>"
 
     # print(stripped)
     f = open("../blog.html", "w")
@@ -233,42 +257,54 @@ def addEntrytoHTML():
     # run prettier on file.
     os.system("npx prettier --write ../blog.html")
     os.system("npx prettier --write ./tags.json")
+    os.system("make")
 
 
 def addTags(template):
     global added_new_tag
 
+    # checks if there are tags to add
     if (len(tags) > 0):
+        # opens the tags.json file to read
         json_file = open("tags.json", "r")
         data = json_file.read()
+        # hold all the objects in file
         objs = json.loads(data)
         json_file.close()
 
+        newtag = ""
+        # iterate through all the tags
         for tag in tags:
+            # clean the tag from any leading or trailing whitespace
             tag = tag.strip()
 
+            # instantiate a tag_color variable
             tag_color = "#FFFFFF"
 
             keyfound = False
             for key in objs:
+                # check if tag already exists
                 if (key.upper() == tag.upper()):
                     keyfound = True
                     tag_color = objs[key]
+                    # create the tag spam html code
                     newtag = "<span class='uk-label' style='background-color: {}'>{}</span>\n".format(
                         tag_color, tag)
 
+            # if tag doesn't exist, add it to the json file
             if (not keyfound):
-                # random hex color
+                # give it a random hex color
                 random_number = random.randint(0, 16777215)
                 hex_number = format(random_number, 'x')
                 hex_number = '#'+hex_number
 
+                # write the new tag to the json file
                 objs[tag] = hex_number
                 tag_color = objs[tag]
-                print(objs)
                 json_file = open("tags.json", "w+")
                 json_obj = json.dumps(objs, indent=2)
                 json_file.write(str(json_obj))
+                # create the tag spam html code
                 newtag = "<span class='uk-label' style='background-color: {}'>{}</span>\n".format(
                     tag_color, tag)
 
